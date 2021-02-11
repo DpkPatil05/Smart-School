3e0c6e82d2fc7ba2260db3083eefb9b99fef2bee
vishaal <vishnair001@gmail.com>
2021-02-11 06:29:58 +0530

    UI done for transport and teacher review.

--- lib/tabs/hamItems/teachers_review.dart
+++ lib/tabs/hamItems/teachers_review.dart
@@ -9,7 +9,78 @@ class _TeachersReviewState extends State<TeachersReview> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Apply Leave"),
+      color: Colors.white,
+      child: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            // final homeworkData = homework[index];
+            return Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: ListTile(
+                title: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.person_sharp),
+                        Text(
+                          'Teachers Name',
+                          style: TextStyle(
+                              fontSize: 24.0
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.call),
+                            SizedBox(width: 10.0),
+                            Text(
+                                'From server'
+                            ),
+                          ],
+                        ),
+                        Container(
+                          height: 30.0,
+                          child: IconButton(
+                            iconSize: 20.0,
+                            icon: Icon(Icons.preview_outlined),
+                            tooltip: "View Details",
+                            onPressed: () => showModalBottomSheet(
+                                context: context,
+                                builder: (BuildContext context) => Container(
+                                  alignment: Alignment.center,
+                                  height: 300.0,
+                                  child: Text('Details about the Notice'),
+                                )
+                            ),
+                            // child: const Text('View'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.email),
+                        SizedBox(width: 10.0),
+                        Text(
+                            'From server'
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            );
+          }
+          )
     );
   }
 }
+
--- lib/tabs/hamItems/transport_route.dart
+++ lib/tabs/hamItems/transport_route.dart
@@ -9,7 +9,67 @@ class _TransportRouteState extends State<TransportRoute> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Apply Leave"),
+        color: Colors.white,
+        child: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, index) {
+              // final homeworkData = homework[index];
+              return Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: ListTile(
+                    title: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.location_on_outlined),
+                            Text(
+                              'Route title',
+                              style: TextStyle(
+                                  fontSize: 24.0
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(Icons.directions_bus_outlined),
+                                SizedBox(width: 10.0),
+                                Text(
+                                    'From server'
+                                ),
+                              ],
+                            ),
+                            Container(
+                              height: 30.0,
+                              child: IconButton(
+                                iconSize: 20.0,
+                                icon: Icon(Icons.preview_outlined),
+                                tooltip: "View Details",
+                                onPressed: () => showModalBottomSheet(
+                                    context: context,
+                                    builder: (BuildContext context) => Container(
+                                      alignment: Alignment.center,
+                                      height: 300.0,
+                                      child: Text('Details about the Notice'),
+                                    )
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+              );
+            }
+        )
     );
   }
 }
