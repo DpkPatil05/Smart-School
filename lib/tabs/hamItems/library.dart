3ddf3333892e0e4b18efccae2486631f78528a7d
DpkPatil05 <patildeepak188@gmail.com>
2021-02-12 10:21:26 +0530

    Library done(Due date and status pending)

--- lib/tabs/hamItems/library.dart
+++ lib/tabs/hamItems/library.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smart_school/modal/library.dart';
+import 'package:smart_school/templates/library.dart';
 
 class Library extends StatefulWidget {
   final List<LibraryData> librarydata;
@@ -15,101 +16,9 @@ class _LibraryState extends State<Library> {
     return ListView.builder(
         itemCount: 3,
         itemBuilder: (context, index) {
-          return Card(
-            elevation: 5,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: ListTile(
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Subject name',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold
-                              ),
-                            ),
-                            Text(
-                              'Author name',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10.0),
-                    Row(
-                      children: [
-                        Table(
-                          defaultColumnWidth: FixedColumnWidth(90.0),
-                          children: [
-                            TableRow(children: [
-                              Text(
-                                'Book No:',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                ),
-                              ),
-                              Text('server'),
-                            ]),
-                            TableRow(children: [
-                              Text(
-                                'Issue Date',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                ),
-                              ),
-                              Text('server'),
-                            ]),
-                            TableRow(children: [
-                              Text(
-                                'Return Date',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                ),
-                              ),
-                              Text('server'),
-                            ]),
-                          ],
-                        ),
-                        Table(
-                          defaultColumnWidth: FixedColumnWidth(90.0),
-                          children: [
-                            TableRow(children: [
-                              Text(
-                                'Due Date',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                ),
-                              ),
-                              Text('server'),
-                            ]),
-                            TableRow(children: [
-                              Text(
-                                'status',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                ),
-                              ),
-                              Text('server'),
-                            ]),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-            ),
-          );
+          return LibraryCard(librarydata: widget.librarydata[index]);
         }
     );
   }
 }
+
---
+++ lib/templates/library.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:smart_school/modal/library.dart';
+
+class LibraryCard extends StatefulWidget {
+  final LibraryData librarydata;
+
+  const LibraryCard({Key key, this.librarydata}) : super(key: key);
+
+
+  @override
+  _LibraryCardState createState() => _LibraryCardState();
+}
+
+class _LibraryCardState extends State<LibraryCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: ListTile(
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${widget.librarydata.bookTitle}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Text(
+                        '${widget.librarydata.author}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10.0),
+              Row(
+                children: [
+                  Table(
+                    defaultColumnWidth: FixedColumnWidth(90.0),
+                    children: [
+                      TableRow(children: [
+                        Text(
+                          'Book No:',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text('${widget.librarydata.bookNo}'),
+                      ]),
+                      TableRow(children: [
+                        Text(
+                          'Issue Date:',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text('${widget.librarydata.issueDate}'),
+                      ]),
+                      TableRow(children: [
+                        Text(
+                          'Return Date',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text('${widget.librarydata.returnDate}'),
+                      ]),
+                    ],
+                  ),
+                  // Table(
+                  //   defaultColumnWidth: FixedColumnWidth(90.0),
+                  //   children: [
+                  //     TableRow(children: [
+                  //       Text(
+                  //         'Due Date',
+                  //         style: TextStyle(
+                  //             fontWeight: FontWeight.bold
+                  //         ),
+                  //       ),
+                  //       Text('${widget.librarydata}'),
+                  //     ]),
+                  //     TableRow(children: [
+                  //       Text(
+                  //         'status',
+                  //         style: TextStyle(
+                  //             fontWeight: FontWeight.bold
+                  //         ),
+                  //       ),
+                  //       Text('server'),
+                  //     ]),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          )
+      ),
+    );
+  }
+}
