13c269f00ae8b524891a854e623ba4c7fa273a5c
vishaal <vishnair001@gmail.com>
2021-02-19 17:02:43 +0530

    Library ui change

--- lib/tabs/hamItems/library.dart
+++ lib/tabs/hamItems/library.dart
@@ -10,26 +10,91 @@ class Library extends StatefulWidget {
   _LibraryState createState() => _LibraryState();
 }
 
-class _LibraryState extends State<Library> {
+class _LibraryState extends State<Library>  with SingleTickerProviderStateMixin{
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 2, vsync: this);
+  }
   @override
   Widget build(BuildContext context) {
-    return 1 == widget.librarydata.length ?
-    Card(
-      child: Center(
-          child: Text(
-            "No Books available",
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 15.0
+    return  Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(color: Colors.red),
+            child: TabBar(
+              controller: _controller,
+              tabs: [
+                Tab(
+                  text: 'Books',
+                ),
+                Tab(
+                  text: 'Books Issued',
+                ),
+              ],
             ),
+          ),
+          Container(
+              height: 400.0,
+              child: TabBarView(
+                  controller: _controller,
+                  children: <Widget>[
+                    Container(
+                      child: ListView.builder(
+                          itemCount: 1,
+                          itemBuilder: (context, index) {
+                            return 1 == widget.librarydata.length ?
+                            Center(
+                              heightFactor: 30.0,
+                                child: Text(
+                                  "No Books available",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15.0
+                                  ),
+                                )
+                            ):ListView.builder(
+                                itemCount: widget.librarydata.length-1??0,
+                                itemBuilder: (context, index) {
+                                  return LibraryCard(librarydata: widget.librarydata[index]);
+                                }
+                            );
+                          }
+                      ),
+                    ),
+                    Container(
+                      child: ListView.builder(
+                          itemCount: 1,
+                          itemBuilder: (context, index) {
+                            return 1 == widget.librarydata.length ?
+                            Center(
+                              heightFactor: 30.0,
+                                child: Text(
+                                  "No Books Issued",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15.0
+                                  ),
+                                )
+                            ):ListView.builder(
+                                itemCount: widget.librarydata.length-1??0,
+                                itemBuilder: (context, index) {
+                                  return LibraryCard(librarydata: widget.librarydata[index]);
+                                }
+                            );
+                          }
+                      ),
+                    ),
+                  ]
+              )
           )
-      ),
-    ):ListView.builder(
-        itemCount: widget.librarydata.length-1??0,
-        itemBuilder: (context, index) {
-          return LibraryCard(librarydata: widget.librarydata[index]);
-        }
+        ]
     );
+
+
+
   }
 }
 
