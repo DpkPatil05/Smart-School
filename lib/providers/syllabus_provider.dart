ea543baa9b7ef255e8c048871fef32984545a91f
DpkPatil05 <patildeepak188@gmail.com>
2021-02-28 21:20:29 +0530

    syllabus percentage complete

--- lib/providers/syllabus_provider.dart
+++ lib/providers/syllabus_provider.dart
@@ -7,6 +7,10 @@ import 'package:smart_school/modal/syllabus.dart';
 class SyllabusProvider with ChangeNotifier {
   String url = '';
 
+  double subjectCompletion = 0.0, topicsCompletion = 0.0;
+
+  int total = 0;
+
   toast(String msg) {
     Fluttertoast.showToast(
         msg: '$msg',
@@ -16,6 +20,29 @@ class SyllabusProvider with ChangeNotifier {
         textColor: Colors.white,
         fontSize: 16.0
     );
+  }
+
+  // ignore: missing_return
+  Future<List<SyllabusData>> fetchSyllabus(String url) async {
+    print('Syllabus data url: ' + url);
+    bool result = await DataConnectionChecker().hasConnection;
+    if (result) {
+      try {
+        final response = await http.get(url);
+        if (response.statusCode == 200) {
+          // If the server did return a 200 OK response,
+          // then parse the JSON.=
+          final List<SyllabusData> syllabusData = syllabusDataFromJson(response.body);
+          return syllabusData;
+        } else {
+          // If the server did not return a 200 OK response,
+          // then throw an exception.
+          toast("Problem fetching data");
+        }
+      } catch(e) {
+        toast("Problem fetching data");
+      }
+    } else toast("No Data connection");
   }
 
   int lessonsCount(String subject, List<SyllabusData> lessons) {
@@ -47,6 +74,8 @@ class SyllabusProvider with ChangeNotifier {
   getDetails(String subject, String lesson,
       List<SyllabusData>topics) {
     List topicsData = [];
+    int complete = 0, total = 0;
+    double totalCompletion = 0.0;
 
     for(int i=0; i<topics.length; i++) {
       if (topics[i].subject == subject && topics[i].lesson == lesson) {
@@ -55,33 +84,15 @@ class SyllabusProvider with ChangeNotifier {
         topicDetails[1] = topics[i].completion;
         topicDetails[2] = topics[i].date;
         topicsData.add(topicDetails);
+        total = total+1;
+        if(topicDetails[1] == 'Complete') complete = complete+1;
       }
     }
+
+    if(complete == 0) totalCompletion = 0;
+    else totalCompletion = (complete/total) * 100;
+    topicsData.add(totalCompletion);
     return topicsData;
   }
-
-  // ignore: missing_return
-  Future<List<SyllabusData>> fetchSyllabus(String url) async {
-    print('Syllabus data url: ' + url);
-    bool result = await DataConnectionChecker().hasConnection;
-    if (result) {
-      try {
-        final response = await http.get(url);
-        if (response.statusCode == 200) {
-          // If the server did return a 200 OK response,
-          // then parse the JSON.=
-          final List<SyllabusData> syllabusData = syllabusDataFromJson(response.body);
-          return syllabusData;
-        } else {
-          // If the server did not return a 200 OK response,
-          // then throw an exception.
-          toast("Problem fetching data");
-        }
-      } catch(e) {
-        toast("Problem fetching data");
-      }
-    } else toast("No Data connection");
-  }
-
 
 }
--- lib/templates/syllabus.dart
+++ lib/templates/syllabus.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:smart_school/modal/syllabus.dart';
 import 'package:smart_school/providers/syllabus_provider.dart';
 
@@ -15,6 +16,7 @@ class SyllabusCard extends StatefulWidget {
 }
 
 class _SyllabusCardState extends State<SyllabusCard> {
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -22,136 +24,169 @@ class _SyllabusCardState extends State<SyllabusCard> {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15.0),
         ),
-        child: ListTile(
-          title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children:[
-                Text('${widget.subject.subject}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17.0
+        child: Consumer<SyllabusProvider>(
+          builder: (BuildContext context, syllabusProv, child) {
+            return ListTile(
+            title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:[
+                  Text('${widget.subject.subject} (%)',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17.0
+                    ),
                   ),
-                ),
-                IconButton(
-                  iconSize: 20.0,
-                  icon: Icon(Icons.preview_outlined),
-                  tooltip: "View Details",
-                  onPressed: () => showModalBottomSheet(
-                      context: context,
-                      builder: (BuildContext context) => ListView.builder(
-                        itemCount: SyllabusProvider()
-                            .lessonsCount(widget.subject.subject, widget.lessons),
-                        itemBuilder: (BuildContext context, int index0) {
-                          var topicData = SyllabusProvider()
-                              .getDetails(widget.subject.subject,
-                              widget.lessons[index0].lesson,
-                              widget.topics);
-                          return ExpansionTile(
-                            title: Text(
-                              '${widget.lessons[index0].lesson}',
-                              style: TextStyle(
-                                  fontSize: 17.0,
-                                  fontWeight: FontWeight.bold
-                              ),
-                            ),
-                            children: [
-                              Row(
-                                children: <Widget>[
-                                  Expanded(
-                                    child: SizedBox(
-                                      height: 240.0,
-                                      child: 0 == SyllabusProvider()
-                                          .topicsCount(widget.subject.subject,
-                                          widget.lessons[index0].lesson,
-                                          widget.topics) ?
-                                          Center(child: Text('No data',
-                                              style: TextStyle(
-                                                  fontSize: 17.0,
-                                                  fontWeight: FontWeight.bold)
-                                          )) : ListView.builder(
-                                          itemCount: SyllabusProvider().topicsCount(
-                                              widget.subject.subject,
-                                              widget.lessons[index0].lesson,
-                                              widget.topics),
-                                          itemBuilder: (BuildContext context,
-                                              int index1) {
-                                            return ListBody(
-                                              children: [
-                                                Row(
-                                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                                  children: [
-                                                    Padding(
-                                                      padding: const EdgeInsets
-                                                          .only(left: 14.0),
-                                                      child: SizedBox(
-                                                          width: 160.0,
-                                                          child: Text('Topic:')
-                                                      ),
-                                                    ),
-                                                    Text('${topicData[index1][0]}'),
-                                                    SizedBox(width: 5.0),
-                                                  ],
-                                                ),
-                                                SizedBox(height: 5.0),
-                                                Row(
-                                                  mainAxisAlignment: MainAxisAlignment
-                                                      .spaceBetween,
-                                                  children: [
-                                                    Padding(
-                                                      padding: const EdgeInsets
-                                                          .only(left: 14.0),
-                                                      child: SizedBox(
-                                                          width: 180.0,
-                                                          child: Text('Status:')
-                                                      ),
-                                                    ),
-                                                    'Complete' == topicData[index1][1] ?
-                                                    Text('${topicData[index1][1]}',
-                                                        style: TextStyle(
-                                                            color: Colors.green))
-                                                        : Text(
-                                                        '${topicData[index1][1]}',
-                                                        style: TextStyle(
-                                                            color: Colors.red)),
-                                                    SizedBox(width: 5.0),
-                                                  ],
-                                                ),
-                                                SizedBox(height: 5.0),
-                                                Row(
-                                                  mainAxisAlignment: MainAxisAlignment
-                                                      .spaceBetween,
-                                                  children: [
-                                                    Padding(
-                                                      padding: const EdgeInsets
-                                                          .only(left: 14.0),
-                                                      child: SizedBox(
-                                                          width: 180.0,
-                                                          child: Text(
-                                                              'Completion Date:')
-                                                      ),
-                                                    ),
-                                                    Text('${topicData[index1][2]}'),
-                                                    SizedBox(width: 5.0),
-                                                  ],
-                                                ),
-                                                Divider(thickness: 3.0),
-                                                SizedBox(height: 25.0),
-                                              ],
-                                            );
-                                          }
+                  IconButton(
+                    iconSize: 20.0,
+                    icon: Icon(Icons.preview_outlined),
+                    tooltip: "View Details",
+                    onPressed: () => showModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) => ListView.builder(
+                          itemCount: syllabusProv
+                              .lessonsCount(widget.subject.subject, widget.lessons),
+                          itemBuilder: (BuildContext context, int index0) {
+                            var topicData = syllabusProv
+                                .getDetails(widget.subject.subject,
+                                widget.lessons[index0].lesson,
+                                widget.topics);
+                            return ExpansionTile(
+                              title: 0 == syllabusProv.topicsCount(
+                                  widget.subject.subject,
+                                  widget.lessons[index0].lesson,
+                                  widget.topics) ?
+                                Row(
+                                  children: [
+                                    Text(
+                                    '${widget.lessons[index0].lesson}',
+                                    style: TextStyle(
+                                      fontSize: 17.0,
+                                      fontWeight: FontWeight.bold
+                                    )),
+                                    SizedBox(width: 5.0),
+                                    Expanded(
+                                      child: Text(
+                                          '(No status)',
+                                          style: TextStyle(
+                                              color: Colors.red,
+                                            fontSize: 17.0,
+                                            fontWeight: FontWeight.bold
+                                          )
                                       ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ) : Text(
+                                '${widget.lessons[index0].lesson} (${topicData[
+                                  syllabusProv.topicsCount(
+                                    widget.subject.subject,
+                                    widget.lessons[index0].lesson,
+                                    widget.topics)]}%)',
+                                style: TextStyle(
+                                    fontSize: 17.0,
+                                    fontWeight: FontWeight.bold
+                                ),
                               ),
-                            ],
-                          );
-                        }
-                      ),
+                              children: [
+                                Row(
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: SizedBox(
+                                        height: 240.0,
+                                        child: 0 == syllabusProv
+                                            .topicsCount(widget.subject.subject,
+                                            widget.lessons[index0].lesson,
+                                            widget.topics) ?
+                                            Center(child: Text('No data',
+                                                style: TextStyle(
+                                                    fontSize: 17.0,
+                                                    fontWeight: FontWeight.bold)
+                                            )) : ListView.builder(
+                                            itemCount: syllabusProv.topicsCount(
+                                                widget.subject.subject,
+                                                widget.lessons[index0].lesson,
+                                                widget.topics),
+                                            itemBuilder: (BuildContext context,
+                                                int index1) {
+                                              return ListBody(
+                                                children: [
+                                                  Row(
+                                                    mainAxisAlignment: MainAxisAlignment
+                                                        .spaceBetween,
+                                                    children: [
+                                                      Padding(
+                                                        padding: const EdgeInsets
+                                                            .only(left: 14.0),
+                                                        child: SizedBox(
+                                                            width: 160.0,
+                                                            child: Text('Topic:')
+                                                        ),
+                                                      ),
+                                                      Text('${topicData[index1][0]}'),
+                                                      SizedBox(width: 5.0),
+                                                    ],
+                                                  ),
+                                                  SizedBox(height: 5.0),
+                                                  Row(
+                                                    mainAxisAlignment: MainAxisAlignment
+                                                        .spaceBetween,
+                                                    children: [
+                                                      Padding(
+                                                        padding: const EdgeInsets
+                                                            .only(left: 14.0),
+                                                        child: SizedBox(
+                                                            width: 180.0,
+                                                            child: Text('Status:')
+                                                        ),
+                                                      ),
+                                                      'Complete' == topicData[index1][1] ?
+                                                      Text('${topicData[index1][1]}',
+                                                          style: TextStyle(
+                                                              color: Colors.green))
+                                                          : Text(
+                                                          '${topicData[index1][1]}',
+                                                          style: TextStyle(
+                                                              color: Colors.red)),
+                                                      SizedBox(width: 5.0),
+                                                    ],
+                                                  ),
+                                                  SizedBox(height: 5.0),
+                                                  Row(
+                                                    mainAxisAlignment: MainAxisAlignment
+                                                        .spaceBetween,
+                                                    children: [
+                                                      Padding(
+                                                        padding: const EdgeInsets
+                                                            .only(left: 14.0),
+                                                        child: SizedBox(
+                                                            width: 180.0,
+                                                            child: Text(
+                                                                'Completion Date:')
+                                                        ),
+                                                      ),
+                                                      Text('${topicData[index1][2]}'),
+                                                      SizedBox(width: 5.0),
+                                                    ],
+                                                  ),
+                                                  Divider(thickness: 3.0),
+                                                  SizedBox(height: 25.0),
+                                                ],
+                                              );
+                                            }
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            );
+                          }
+                        ),
+                    ),
                   ),
-                ),
-              ]
-          ),
+                ]
+            ),
+          );
+          },
         )
     );
   }
