2bcb67caf9f2ea1d3a5c71303c5611290f22b7ce
DpkPatil05 <patildeepak188@gmail.com>
2021-03-09 14:37:49 +0530

    assignment heading added

--- lib/tabs/hamItems/download_center.dart
+++ lib/tabs/hamItems/download_center.dart
@@ -23,64 +23,84 @@ void initState() {
   Widget build(BuildContext context) {
     return
       Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(color: Colors.red),
-          child: TabBar(
-            controller: _controller,
-            tabs: [
-              Tab(
-                text: 'Assignments',
-              ),
-              Tab(
-                text: 'Study Material',
-              ),
-              Tab(
-                text: 'Syllabus',
-              ),
-              Tab(
-                text: 'Others',
-              ),
-            ],
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            decoration: BoxDecoration(color: Colors.red),
+            child: TabBar(
+              controller: _controller,
+              tabs: [
+                Tab(text: 'Assignments'),
+                Tab(text: 'Study Material'),
+                Tab(text: 'Syllabus'),
+                Tab(text: 'Others'),
+              ],
+            ),
           ),
-        ),
         Container(
-          height: 400.0,
+            height: 600.0,
             child: TabBarView(
               controller: _controller,
               children: <Widget>[
                 Container(
                   child: 'nodata'!=widget.downloadcenterdata[0][0].title?
-                  ListView.builder(
-                      itemCount: widget.downloadcenterdata[0]?.length??0,
-                      itemBuilder: (context, index) {
-                        return Card(
-                            elevation: 5,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              height: 50.0,
+                              color: Colors.grey,
+                              child: Center(child: Text(
+                                'Assignment',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17.0
+                                ),
+                              ))
                             ),
-                            child: ListTile(
-                              title: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  SizedBox(
-                                    width: 200.0,
-                                    child: Text('${widget.downloadcenterdata[0][index].title}')
+                          ),
+                      ]),
+                      Container(
+                        height: 500.0,
+                        child: ListView.builder(
+                            itemCount: widget.downloadcenterdata[0]?.length??0,
+                            itemBuilder: (context, index) {
+                              return Card(
+                                  elevation: 5,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15.0),
                                   ),
-                                  Text('${widget.downloadcenterdata[0][index].date}'),
-                                  IconButton(
-                                      icon: Icon(Icons.file_download),
-                                      onPressed: (){
-                                        CheckPermissions().checkStoragePermission().then((value) =>
-                                            DownloadCenterProvider().generateDownload(widget.downloadcenterdata[0][index].link));
-                                      }
-                                  ),
-                                  SizedBox(width: 5)
-                                ],
-                              )
-                            )
-                        );
-                      }
+                                  child: ListTile(
+                                    title: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        SizedBox(
+                                          width: 200.0,
+                                          child: Text('${widget.downloadcenterdata[0][index].title}')
+                                        ),
+                                        Text('${widget.downloadcenterdata[0][index].date}'),
+                                        IconButton(
+                                            icon: Icon(Icons.file_download),
+                                            onPressed: (){
+                                              CheckPermissions().checkStoragePermission().then((value) =>
+                                                  DownloadCenterProvider().generateDownload(widget.downloadcenterdata[0][index].link));
+                                            }
+                                        ),
+                                        SizedBox(width: 5)
+                                      ],
+                                    )
+                                  )
+                              );
+                            }
+                        ),
+                      ),
+                    ],
                   ) : Card(
                     child: Center(
                         child: Text(
